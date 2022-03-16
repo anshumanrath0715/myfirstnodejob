1623410540655689b25f287f6ffa2dfdfa3420dc
Anshuman Rath <anshuman.rath@gmail.com>
2022-03-16 19:59:16 +0530

    Added express js app, tried multiple easy option for REST Consumer invocation

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Start with Install of Express at 12:00 PM IST
 
 Created the express server and check in 12:07 PM IST and break for Work
 
+Work done during afternoon and evening break 1-2 hrs worth
+
+    Added express js app, tried multiple easy option for REST Consumer invocation 
+
--- server.js
+++ server.js
@@ -1,8 +1,52 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const request = require('request');
 
 app.get('/', function (req, res) {
-  res.send('Hello World, Holla Mundo, Avibadana Viswa')
-})
+  res.send('Hello World, Holla Mundo, Viswa Avibadana :) !!')
+});
 
-app.listen(3000)
+app.get('/movies', (req, res) => {
+  var MOVIE_NAME = req.query.title;
+  request('https://api.themoviedb.org/3/search/movie?api_key=41a6894ca93cb1c78657d9e799e164de&query=' + MOVIE_NAME, { json: true }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
+  });
+});
+
+app.get('/movies/:movie_id', (req, res) => {
+  var MOVIE_ID = req.params['movie_id'];
+  console.log('Looking for Movie ID: ' + MOVIE_ID);
+  request('https://api.themoviedb.org/3/movie/' + MOVIE_ID + '?api_key=41a6894ca93cb1c78657d9e799e164de', { json: true }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
+  });
+});
+
+app.get('/shows', (req, res) => {
+  var SHOW_NAME = req.query.name;
+  request('https://api.themoviedb.org/3/search/tv?api_key=41a6894ca93cb1c78657d9e799e164de&query=' + SHOW_NAME, { json: true }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
+  });
+});
+
+app.get('/shows/:show_id', (req, res) => {
+  var SHOW_ID = req.params['show_id'];
+  console.log('Looking for Show ID: ' + SHOW_ID);
+  request('https://api.themoviedb.org/3/tv/' + SHOW_ID + '?api_key=41a6894ca93cb1c78657d9e799e164de', { json: true }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`)
+})
